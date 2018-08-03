778d34aa4364250fc4536197e7b6c0c2bfadfa2c
bhanuchanderu <bhanuchanderu@nmsworks.co.in>
2018-08-03 12:30:55 +0530

    modified..

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# epfis
-- epfis with arduino
+## Electronic Primary Fuel Injection System
+<hr>
+
+Simple code for controlling the pulse to the Electronic fuel injector from 
+the reference of engine TDC Position which should be in 5 bar pressure powered 
+by a pump for the dual fuel automobile engine.
+
+### Apparatus
+
+###### ECU
+
+- Arduino Board
+- Laptop with arduio installed laptop
+
+##### Engine
+
+- Dual fuel engine with proper coolant system
+- TDC Position sensor (Proximity sensor)
+- Electronic Fuel injector
+- Fuel Electrical Pump
+
+###### Measurement
+
+- DSO (Digital Storage Oscilloscope)
+- Graduated cylinders / Volumetric flask 
+
+### Generated Test Pulse
+
+// TODO Image need to be rendered.
+
--- test_cycle.ino
+++ test_cycle.ino
@@ -1,49 +1,69 @@
+// Note:
+// Test cycle code is used for measuring the fuel injection amount
+// for a ON Time. It means to measure Fuel Injection rate (volume/s) in
+// steady state condition (Engine off).
 
-const int buttonPin = 3;     // the number of the pushbutton pin
+
+// In this code proximity sensory input is used as a switch to on/off the
+// experiment.
+
+const int buttonPin = 3;     // the number of the TDC_Input pin
 const int ledPin13 =  13;      // the number of the LED pin
 const int ledPin12 =  12;      // the number of the LED pin
-
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState = 0;         // variable for reading the TDC_Input status
 int n =0;
+
 void setup() {
+
   Serial.begin(9600);
+
   // initialize the LED pin as an output:
   pinMode(ledPin13, OUTPUT);
   pinMode(ledPin12, OUTPUT);
-  // initialize the pushbutton pin as an input:
+
+  // initialize the TDC_Input pin as an input:
   pinMode(buttonPin, INPUT);
+
 }
 
 void loop() 
 {
-    // read the state of the pushbutton value:
-      int buttonState = digitalRead(buttonPin);
-      
+    // read the state of the TDC_Input value:
+    int buttonState = digitalRead(buttonPin);
 
-  if ( buttonState == HIGH ) 
-  
-  {
-  for (int i=0; i <= 100; i++)
-  {
+    if ( buttonState == HIGH )
+    {
+        for (int i=0; i <= 100; i++)
+        {
+            Serial.println("1st pulse");
+            // Put delay for First revolution
+            // delay(40)
+            // First revolution completed.
 
-           Serial.println("1st pulse");
-          digitalWrite(ledPin13, HIGH);
-          digitalWrite(ledPin12, LOW);
-          Serial.println("delay on");
-          
-             delayMicroseconds(2200);
-           digitalWrite(ledPin12, HIGH);
-           Serial.println(" on time");           
-           delay(15);
-          digitalWrite(ledPin13, LOW);
-          digitalWrite(ledPin12, LOW);
-          delay(80-2.2-15);
-  }    
-      }      
-  else
-  {
-  }
-  
-  
+            // Indicate 2nd Revolution
+            digitalWrite(ledPin13, HIGH);
+            digitalWrite(ledPin12, LOW);
+
+            Serial.println("Valve Overlap Time...");
+            delayMicroseconds(2200);
+
+            // Injecting in second revolution
+            digitalWrite(ledPin12, HIGH);
+            Serial.println("Injection Time");
+
+            // Put the ON Time
+            delay(15);
+
+            digitalWrite(ledPin13, LOW);
+            digitalWrite(ledPin12, LOW);
+
+            // Delay need to be provide for OFF Time for Average Engine cycle
+            // time 80 ms. 2.2 ms is for valve overlap time. 15 ms is ON Time.
+            delay(80-2.2-15);
+        }
+    }
+    else
+    {
+        // Nothing to do
+    }
 }
